b31c5f79a4ce77c944c7baf0bd9d2f20e6dfe9cd
Alim Bolar <alim@fourplusmedia.com>
2022-04-22 15:58:17 +0800

    minor fix.. excluded the _id field from the getAll API call for filters

--- controllers/filterController.js
+++ controllers/filterController.js
@@ -3,7 +3,7 @@ const { response } = require("..");
 const Filter = require("./../models/filterModel");
 
 exports.getAllFilters = async (req, res, next) => {
-  const filters = await Filter.find();
+  const filters = await Filter.find({}, { _id: 0 });
   res.status(200).json({
     status: "success",
     data: filters,
